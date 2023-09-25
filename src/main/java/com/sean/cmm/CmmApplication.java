299dec20bba4246952607a861d25b409a370add0
Sean_Xiao <Sean_Xiao@epam.com>
2023-09-25 09:46:40 +0800

    Initial commit

---
+++ src/main/java/com/sean/cmm/CmmApplication.java
@@ -0,0 +1,13 @@
+package com.sean.cmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sean/cmm/CmmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sean.cmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
