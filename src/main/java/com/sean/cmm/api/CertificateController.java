11b9d15563ca4bc02f52b1d8c063f40954c259e7
Sean_Xiao <Sean_Xiao@epam.com>
2023-09-28 15:03:39 +0800

    add interceptor and controller

--- src/main/java/com/sean/cmm/api/CertificateController.java
+++ src/main/java/com/sean/cmm/api/CertificateController.java
@@ -1,4 +1,15 @@
 package com.sean.cmm.api;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/certificate")
 public class CertificateController {
+
+    @PostMapping("/upload")
+    public Object upload(){
+        return null;
+    }
 }
---
+++ src/main/java/com/sean/cmm/web/TokenVerifyInterceptor.java
@@ -0,0 +1,4 @@
+package com.sean.cmm.web;
+
+public class TokenVerifyInterceptor {
+}
