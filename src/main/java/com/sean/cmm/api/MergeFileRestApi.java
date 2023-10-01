ebf70d562ce7acb16f732a3014f5a1130e7e67e0
Sean_Xiao <Sean_Xiao@epam.com>
2023-10-01 23:08:24 +0800

    add MergeFileRestApi

---
+++ src/main/java/com/sean/cmm/api/MergeFileRestApi.java
@@ -0,0 +1,9 @@
+package com.sean.cmm.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/merge-file")
+public class MergeFileRestApi {
+}
