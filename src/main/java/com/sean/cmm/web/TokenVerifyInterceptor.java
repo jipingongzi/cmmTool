70b0a649d24585e00898bb3757098ec0d38b11ff
Sean_Xiao <Sean_Xiao@epam.com>
2023-10-02 10:55:49 +0800

    token interceptor

--- src/main/java/com/sean/cmm/web/TokenVerifyInterceptor.java
+++ src/main/java/com/sean/cmm/web/TokenVerifyInterceptor.java
@@ -1,4 +1,31 @@
 package com.sean.cmm.web;
 
-public class TokenVerifyInterceptor {
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+public class TokenVerifyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) throws Exception {
+        System.out.println("\n----- Token Verify -----");
+        System.out.println("Token: " + request.getHeader("cmm-token"));
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        // Do nothing
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        // Do nothing
+    }
 }
--- src/main/java/com/sean/cmm/web/WebMvcConfig.java
+++ src/main/java/com/sean/cmm/web/WebMvcConfig.java
@@ -7,5 +7,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LogInterceptor());
+        registry.addInterceptor(new TokenVerifyInterceptor());
     }
 }
