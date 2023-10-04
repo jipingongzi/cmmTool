e72a6adf190e9c4fc6b7819edd559daedf05a290
sean xiao <sean_xiao@epam.com>
2023-10-04 23:16:32 +0800

    create util class for future

--- src/main/java/com/sean/cmm/util/OssUtil.java
+++ src/main/java/com/sean/cmm/util/OssUtil.java
@@ -4,10 +4,5 @@ import java.io.File;
 
 public final class OssUtil {
 
-    public static void upload(File file){}
-
-    public static Object getMetaData(String url){
-        return null;
-    }
 
 }
