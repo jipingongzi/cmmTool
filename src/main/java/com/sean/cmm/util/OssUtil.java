e5d490b2e2af3185155b26ef984ec8b2cd85e899
sean xiao <435032183@qq.com>
2023-10-06 23:19:45 +0800

    add empty methods for OssUtil

--- src/main/java/com/sean/cmm/util/OssUtil.java
+++ src/main/java/com/sean/cmm/util/OssUtil.java
@@ -1,8 +1,22 @@
 package com.sean.cmm.util;
 
 import java.io.File;
+import java.util.List;
 
 public final class OssUtil {
+    public static String upload(String bucketName, File file){
+        return "";
+    }
+    public static String getFileUrl(String bucketName, String fileName){
+        return "";
+    }
 
+    public static Byte[] downLoad(String fileUrl){
+        return null;
+    }
+
+    public static List<String> getFileUrls(String bucketName){
+        return null;
+    }
 
 }
