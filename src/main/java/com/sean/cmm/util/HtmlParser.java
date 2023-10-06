02f829441f118fd080a5bcd11d761844f37fc4c9
sean xiao <435032183@qq.com>
2023-10-06 14:07:36 +0800

    create util class for future

--- src/main/java/com/sean/cmm/util/HtmlParser.java
+++ src/main/java/com/sean/cmm/util/HtmlParser.java
@@ -1,8 +1,13 @@
 package com.sean.cmm.util;
 
+import java.util.List;
+
 public class HtmlParser {
 
     public static String getElementData(String id){
         return "";
+    }
+    public static List<String> getElementDataByTag(String tag){
+        return null;
     }
 }
