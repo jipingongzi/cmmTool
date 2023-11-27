45924e7e3a20937a260d476fb7d31d5956edb9e7
sean xiao <435032183@qq.com>
2023-11-27 11:04:37 +0800

    update elevator logic

--- src/main/java/com/sean/cmm/plugin/elevator/model/Elevator.java
+++ src/main/java/com/sean/cmm/plugin/elevator/model/Elevator.java
@@ -2,25 +2,43 @@ package com.sean.cmm.plugin.elevator.model;
 
 import com.sean.cmm.plugin.elevator.config.Config;
 
-import java.util.PriorityQueue;
 import java.util.Comparator;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Elevator implements Runnable {
-    private final PriorityQueue<Integer> upTasks;
-    private final PriorityQueue<Integer> downTasks;
+    private static final AtomicInteger count = new AtomicInteger(1);
+    private Integer id;
+    private final PriorityBlockingQueue<Integer> upTasks;
+    private final PriorityBlockingQueue<Integer> downTasks;
     private final LinkedBlockingQueue<Integer> newTasks;
+    private volatile Boolean waitTime;
     private int currentFloor;
 
     public Elevator(int startingFloor) {
-        upTasks = new PriorityQueue<>();
-        downTasks = new PriorityQueue<>(Comparator.reverseOrder());
-        this.newTasks = new LinkedBlockingQueue<>();
-        this.currentFloor = startingFloor;
+        id = count.getAndAdd(1);
+        upTasks = new PriorityBlockingQueue<>();
+        downTasks = new PriorityBlockingQueue<>(10, Comparator.reverseOrder());
+        newTasks = new LinkedBlockingQueue<>();
+        currentFloor = startingFloor;
+        waitTime = false;
+    }
+
+    public void addWait() {
+        waitTime = true;
     }
 
     public void addTask(int floor) {
         try {
+            if (waitTime) {
+                // wait all task finished
+                while (!newTasks.isEmpty() || !upTasks.isEmpty() || !downTasks.isEmpty()) {
+                    Thread.sleep(1000L);
+                }
+                waitTime = false;
+            }
             if (currentFloor != floor &&
                     !newTasks.contains(floor) &&
                     !upTasks.contains(floor) &&
@@ -32,34 +50,45 @@ public class Elevator implements Runnable {
         }
     }
 
+
+    private void listen(){
+        while (true){
+            if(!newTasks.isEmpty()){
+                int nextTask = newTasks.poll();
+                if (nextTask > currentFloor) {
+                    upTasks.offer(nextTask);
+                } else if (nextTask < currentFloor) {
+                    downTasks.offer(nextTask);
+                }
+            }
+        }
+    }
+
     @Override
     public void run() {
+        CompletableFuture.runAsync(this::listen);
         while (true) {
             try {
-                while (!newTasks.isEmpty()) {
-                    int nextTask = newTasks.poll();
-                    if (nextTask > currentFloor) {
-                        upTasks.offer(nextTask);
-                    } else if (nextTask < currentFloor) {
-                        downTasks.offer(nextTask);
-                    }
-                }
-
                 boolean isMovingUp = !upTasks.isEmpty();
                 boolean isMovingDown = !isMovingUp && !downTasks.isEmpty();
-
                 if (isMovingUp) {
-                    currentFloor = upTasks.poll();
-                    System.out.println("Elevator moving up, now at floor: " + currentFloor);
-                    Thread.sleep(Config.getSpeed());
+                    int distance = upTasks.peek() - currentFloor;
+                    for (int i = 0; i < distance; i++) {
+                        Thread.sleep(Config.getSpeed());
+                        currentFloor++;
+                    }
+                    upTasks.poll();
+                    System.out.println("Elevator " + id + " moving up, now at floor: " + currentFloor);
                 }
-
                 if (isMovingDown) {
-                    currentFloor = downTasks.poll();
-                    System.out.println("Elevator moving down, now at floor: " + currentFloor);
-                    Thread.sleep(Config.getSpeed());
+                    int distance = currentFloor - downTasks.peek();
+                    for (int i = 0; i < distance; i++) {
+                        Thread.sleep(Config.getSpeed());
+                        currentFloor--;
+                    }
+                    downTasks.poll();
+                    System.out.println("Elevator " + id + " moving down, now at floor: " + currentFloor);
                 }
-
                 while (isMovingUp && !upTasks.isEmpty() && upTasks.peek() < currentFloor) {
                     newTasks.offer(upTasks.poll());
                 }
@@ -71,4 +100,9 @@ public class Elevator implements Runnable {
             }
         }
     }
+
+    public Integer getId() {
+        return id;
+    }
+
 }
