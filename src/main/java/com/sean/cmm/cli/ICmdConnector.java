9072da672b65ce91e75608ad58fc18050f001db2
sean xiao <Sean_Xiao@epam.com>
2024-01-05 22:07:40 +0800

    local connect

--- src/main/java/com/sean/cmm/cli/ICmdConnector.java
+++ src/main/java/com/sean/cmm/cli/ICmdConnector.java
@@ -1,4 +1,6 @@
 package com.sean.cmm.cli;
 
 public interface ICmdConnector {
+
+    Boolean connect(String url, String username, String pwd);
 }
--- src/main/java/com/sean/cmm/cli/connector/ExamServiceConnector.java
+++ src/main/java/com/sean/cmm/cli/connector/ExamServiceConnector.java
@@ -10,4 +10,18 @@ public class ExamServiceConnector implements ICmdConnector {
 
     @Autowired
     private CertificateService certificateService;
+
+    private final String localUrl = "127.0.0.1";
+    private final String localUserName = "root";
+    private final String localPwd = "123456";
+
+    @Override
+    public Boolean connect(String url, String username, String pwd) {
+        if(localUrl.equals(url)){
+            return localUserName.equals(username) && localPwd.equals(pwd);
+        }
+        // todo remote LDAP check
+        return false;
+
+    }
 }
